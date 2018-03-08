b09a7506f069b6f257ac99155069936432d30873
chasemc67 <chasemc67@gmail.com>
2018-03-07 22:18:05 -0800

    im a god

--- ventureProductScraper/ventureProductScraper/spiders/karcherSite.py
+++ ventureProductScraper/ventureProductScraper/spiders/karcherSite.py
@@ -30,10 +30,16 @@ class KarchersiteSpider(scrapy.Spider):
     def parse(self, response):
         if response.url.find('professional') > -1:
             if response.css('.product-box.product-salesdata'):
-                fullTitle = response.css('.product h1').extract()
-                parsedTitle = re.search('\ {2,}[a-z,A-Z,\-, \ ]+\ {2,}',fullTitle)
-                parsedTitle = parsedTitle.group(0) if parsedTitle else '' 
-                parsedTitle = parsedTitle + re.search('fix-spelling">[a-z,A-Z,\-, \ ]+<', fullTitle).split(">")[1].split("<")[0]
+                fullTitle = response.css('.product h1').extract_first()
+                try:
+                    parsedTitle = re.search('\ {2,}[a-z,A-Z,\-, \ ]+\ {2,}', fullTitle)
+                    if parsedTitle:
+                        parsedTitle = parsedTitle.group(0).strip()
+                    else:
+                        parsedTitle = '' 
+                    parsedTitle = parsedTitle + re.search('fix-spelling.>[a-z,A-Z,\-,\ ,\/,0-9,+,\.,(,),!]+<', fullTitle).group(0).split(">")[1].split("<")[0]
+                except:
+                    parsedTitle = fullTitle
                 
                 yield{
                 #    'image',
@@ -46,39 +52,3 @@ class KarchersiteSpider(scrapy.Spider):
             else:
                 for href in response.css("a::attr('href')"):
                     yield response.follow(href, callback=self.parse)
-
-    def cleanTitle(self, title):
-        return title
-
-    def parse_old(self, response):
-        for category in response.css(".product-item"):
-            categoryName = category.css(".headlinebottom a::text").extract_first()
-            for href in category.css(".headlinebottom a::attr('href')"):
-                yield response.follow(href, callback=self.parseSubCat, meta={'category': categoryName})
-    
-    def parseSubCat(self, response):
-        #open_in_browser(response)
-        #for href in response.css('#products a img').css("a::attr('href')"):
-        for href in response.css('#products a::attr("href")'):
-            yield response.follow(href, callback=self.parseProductsGrid, meta={'category': response.request.meta['category']})
-    
-    def parseProductsGrid(self, response):
-        open_in_browser(response)
-        yield {
-            'category': response.request.meta['category']
-        }
-
-    def checkIfOnProductPages(self, response):
-        #instead of knowing HOW to get to the product pages
-        #just have it visit EVERY page, looking for product pages
-        #and find some way to scope it to only professional products, 
-        #to avoif home and garden
-        if response.url.find('professional') > -1 and response.css('.product-box.product-salesdata'):
-            yield{
-            #    'image',
-                'title': response.css('.product h1').extract()
-                #'description',
-                #'category',
-            #    'subcategories',
-            #    'technical-data'
-            }
