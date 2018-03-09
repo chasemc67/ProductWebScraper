a86ba5b950f0739940cb80bcc7b4ee24a54816c8
chasemc67 <chasemc67@gmail.com>
2018-03-08 22:39:46 -0800

    Working with 4 fields now, next up is images

--- ventureProductScraper/ventureProductScraper/spiders/karcherSite.py
+++ ventureProductScraper/ventureProductScraper/spiders/karcherSite.py
@@ -14,31 +14,44 @@ class KarchersiteSpider(scrapy.Spider):
     # allowed_domains = ['kaercher.com/']
     start_urls = ['https://www.kaercher.com/us/professional.html/']
 
+    def parsedTitleFromResp(self, response):
+        fullTitle = response.css('.product h1').extract_first()
+        parsedTitle = ''
+        try:
+            parsedTitle = re.search('\ {2,}[a-z,A-Z,\-, \ ]+\ {2,}', fullTitle)
+            if parsedTitle:
+                parsedTitle = parsedTitle.group(0).strip()
+            parsedTitle = parsedTitle + " " + re.search('fix-spelling.>[a-z,A-Z,\-,\ ,\/,0-9,+,\.,(,),!]+<', fullTitle).group(0).split(">")[1].split("<")[0]
+        except:
+            parsedTitle = fullTitle
+        return parsedTitle
+
+    def parsedDescFromResp(self, response):
+        description = response.css('#description p').extract_first()
+        description = description.split('<p property=\"description\">')[1].split('</p>')[0]
+        return description
+
+    def parsedCatFromResp(self, response):
+        category = response.css('#breadcrumbs li').extract();
+        category = category[2].split('property=\"name\">')[1].split('</span')[0]
+        return category        
+
+    def parsedSubCatsFromResp(self, response):
+        subCategories = response.css('#breadcrumbs li').extract();
+        finalCats = []
+        for i in range(3,len(subCategories)):
+            finalCats.append(subCategories[i].split('property=\"name\">')[1].split('</span')[0])
+        return str(finalCats)
+
     def parse(self, response):
         if response.url.find('professional') > -1:
             if response.css('.product-box.product-salesdata'):
-                fullTitle = response.css('.product h1').extract_first()
-                try:
-                    parsedTitle = re.search('\ {2,}[a-z,A-Z,\-, \ ]+\ {2,}', fullTitle)
-                    if parsedTitle:
-                        parsedTitle = parsedTitle.group(0).strip()
-                    else:
-                        parsedTitle = '' 
-                    parsedTitle = parsedTitle + " " + re.search('fix-spelling.>[a-z,A-Z,\-,\ ,\/,0-9,+,\.,(,),!]+<', fullTitle).group(0).split(">")[1].split("<")[0]
-                except:
-                    parsedTitle = fullTitle
-
-
-                description = response.css('#description p').extract_first();
-                category = response.css('#breadcrumbs li').extract_first();
-                subCategories = response.css('#breadcrumbs li').extract_first();
-
                 yield{
                 #    'image',
-                    'title': parsedTitle,
-                    'description': description.split('<p property=\"description\">')[1].split('</p>')[0],
-                    'category': category
-                #    'subcategories',
+                    'title': self.parsedTitleFromResp(response),
+                    'description': self.parsedDescFromResp(response),
+                    'category': self.parsedCatFromResp(response),
+                    'subcategories': self.parsedSubCatsFromResp(response)
                 #    'technical-data'
                 }
             else:
